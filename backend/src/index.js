7a114b0a665b168cc69d42b260007c2be8abe4c7
hung30 <95275338+hung30@users.noreply.github.com>
2024-05-14 23:59:20 +0700

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
-  origin: "https://booking-hotel-6568qe85x-hung30s-projects.vercel.app", // Đổi lại thành địa chỉ của trang web của bạn
+  origin: "*", // Đổi lại thành địa chỉ của trang web của bạn
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true, // Cho phép gửi cookie
   allowedHeaders: ["Content-Type"],
